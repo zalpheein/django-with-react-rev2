b75d5dbc9a8c6a50442b2823327a650324edbed0
xalphee <zalphee@gmail.com>
2021-12-29 17:06:46 +0900

    accounts 앱 추가... 추가후 필요한 셋팅까지

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
 
     # Local Apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- askcompany/urls.py
+++ askcompany/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('', TemplateView.as_view(template_name='root.html'), name='root')
 ]
 
@@ -13,5 +14,6 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
+
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
