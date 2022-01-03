7e31e46960e63cf0b92ddcbddccb38cd031bdf00
zalphee <zlaphee@gmail.com>
2022-01-03 23:41:41 +0900

    새로운 필드를 기본 사용자 테이블에 추가하는 방법에 대한 주석...

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+# 장고앱은 User 모델이 반드시 갖추어야 할 모델이 있으므로 AbstractUser 를 사용
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,3 +6,18 @@ from django.db import models
 class User(AbstractUser):
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+
+
+# 새로운 테이블을 만들고 추가 필드를 다음과 같이 정의 할수도 있지만
+# 기본 제공 되는 AbstractUser 를 상속받은 class User 에 위와 같이 필드를 추가 할 수도 있음
+# 이렇게 필드를 추가 할 경우 common.py(기존의 settings.py) 에 다음 항목 추가
+# 보통 DB 관련 이므로... DB 정의부 밑에 선언
+#   AUTH_USER_MODEL = "accounts.User"
+
+# class Profile(models.Model):
+#     pass
+
+
+
+
+
--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -93,8 +93,8 @@ DATABASES = {
     }
 }
 
-
-# 선언 이후에 makemigrations 앱 + migrate 를 반드시 해야 함.
+# accounts 앱에서 추가로 사용자 정보 관련 필드(website_url, boi)를 추가 하였기 때문에 다음과 같이 선언
+# 선언 이후에 반드시 makemigrations 앱 + migrate 를 반드시 해야 함.
 AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
