8e6113b2837bd4d60de0c14dd424b9bcbf962754
zalphee <zlaphee@gmail.com>
2022-01-04 23:53:13 +0900

    사용자 리스트에서 이메일을 노출하여... 중복 여부를 살펴 보기 위햐여...

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,5 +4,6 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    # 사용자 리스트에서 이메일을 노출하여... 중복 여부를 살펴 보기 위햐여.....
     list_display = ['username', 'email', 'website_url', 'is_active', 'is_staff', 'is_superuser']
 
