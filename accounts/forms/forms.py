d7fab4d58f87336accf57869e8526581b037a20f
zalphee <zlaphee@gmail.com>
2022-01-04 23:05:06 +0900

    forms.py 의 SignupForm 클래스 내부에 Meta 클래스 정의...

--- accounts/forms/forms.py
+++ accounts/forms/forms.py
@@ -4,7 +4,10 @@ from ..models import User
 
 
 class SignupForm(UserCreationForm):
-    pass
+    # 기존 UserCreationForm 의 Meta 속성을 오버라잇
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'email', 'first_name', 'last_name']
+
     # class Meta:
     #     model = User
     #     fields = ['username', 'password']
