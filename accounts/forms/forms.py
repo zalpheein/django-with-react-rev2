65a1e837bcd72f98a82a0f134b8a4ead17c35325
zalphee <zlaphee@gmail.com>
2022-01-04 23:11:40 +0900

    회원가입시 필수 입력 필드 지정하기...

--- accounts/forms/forms.py
+++ accounts/forms/forms.py
@@ -4,8 +4,20 @@ from ..models import User
 
 
 class SignupForm(UserCreationForm):
+    # username 을 제외한 나머지 필드들은 unique 항목이 아님..
+    # 그래서 다른 필드들을 unique 로 정의 하는 방법
+    def __call__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+
+
+
+
     # 기존 UserCreationForm 의 Meta 속성을 오버라잇
     class Meta(UserCreationForm.Meta):
+        model = User
         fields = ['username', 'email', 'first_name', 'last_name']
 
     # class Meta:
