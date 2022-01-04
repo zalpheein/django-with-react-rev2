e25a1b72919984e1a32d80d87720fd3ab5075226
zalphee <zlaphee@gmail.com>
2022-01-04 23:00:20 +0900

    forms.py 에 UserCreationForm 을 상속 받은 SignupForm 클래스 생성...class SignupForm(UserCreationForm)...

--- accounts/forms/forms.py
+++ accounts/forms/forms.py
@@ -1,11 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from ..models import User
 
 
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+class SignupForm(UserCreationForm):
+    pass
+    # class Meta:
+    #     model = User
+    #     fields = ['username', 'password']
+
 
 
 
