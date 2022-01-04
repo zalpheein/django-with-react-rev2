8a09e6475c806ded26d95c3f847ae6a9006ba117
zalphee <zlaphee@gmail.com>
2022-01-04 22:35:19 +0900

    views.signup 함수와...templates/accounts/signup_form.html 파일 생성...

--- accounts/forms/forms.py
+++ accounts/forms/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-form ..models import User
+from ..models import User
 
 
 class SignupForm(forms.ModelForm):
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .forms.forms import SignupForm
 
-# Create your views here.
+
+def signup(request):
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "회원가입 환영합니다.")
+            return redirect("/")
+    else:
+        form = SignupForm()
+
+    return render(request, 'accounts/signup_form.html', {
+                        'form': form,
+                    }
+                )
