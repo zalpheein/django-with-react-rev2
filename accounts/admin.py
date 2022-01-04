a8ee6833fb7b6802c3316a2600c1ea8dc3eba7f4
zalphee <zlaphee@gmail.com>
2022-01-04 23:50:51 +0900

    어드민 사용자 리스트 목록에 추가 필드 노출...

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,4 +4,5 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['username', 'email', 'website_url', 'is_active', 'is_staff', 'is_superuser']
+
