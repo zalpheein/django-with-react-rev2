6eccc67ec3ce7879b52e2ab70a89deb5d9569f6b
zalphee <zlaphee@gmail.com>
2022-01-21 22:48:21 +0900

    본 파일 생성 후, accounts/templates/accounts/signup_form.html 파일의 내용을 복붙...

--- instagram/views.py
+++ instagram/views.py
@@ -13,12 +13,12 @@ def post_new(request):
 
             messages.success(request, "포스팅을 저장 하였습니다.")
 
-            # 만약 인자로 post를 사용 하려면, models에 get_absolute_url 함수가 정의 되어야 함
+            # 만약 인자로 post 를 사용 하려면, models에 get_absolute_url 함수가 정의 되어야 함
             return redirect("/")
     else:
         form = PostForm()
 
-    # 당연히 templates/instagram/post_form.html 파일이 존재 해야 함....
+    # 당연히 instagram 앱의 templates/instagram/post_form.html 파일이 존재 해야 함....
     return render(request, "instagram/post_form.html", {
         "form": form,
     })
