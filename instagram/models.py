a4043d9e8583bc553259fa0d412db0f4125aa610
zalphee <zlaphee@gmail.com>
2022-01-21 23:28:19 +0900

    instagram 의 models 에 이미지 업로드 경로를 지정...

--- instagram/models.py
+++ instagram/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField()
+    # 업로드 파일 경로를 정의
+    photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     # 만약 ManyToManyField(Tag, blank=True) 와 같이 표현 하려면... class Tag 를 class Post 이전에 정의
     tag_set = models.ManyToManyField('Tag', blank=True)
