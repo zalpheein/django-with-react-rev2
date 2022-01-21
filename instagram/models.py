a3a73b5867b7ef2c29e6e41f8a3ff69611c253a3
xalphee <zalphee@gmail.com>
2022-01-21 18:03:09 +0900

    instagram 의 models 에 Post와 Tag 모델 정의...

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField()
+    caption = models.CharField(max_length=500)
+    # 만약 ManyToManyField(Tag, blank=True) 와 같이 표현 하려면... class Tag 를 class Post 이전에 정의
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    location = models.CharField(max_length=100, blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
