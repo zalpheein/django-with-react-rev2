eaa2e0a4bc5231dc1f656a1e3afe9daaf7780ed3
xalphee <zalphee@gmail.com>
2022-02-10 17:35:55 +0900

    타임라인 목록에 로그인 사용자가 팔로잉한 사람들의 글 목록 가져오기...

--- instagram/views.py
+++ instagram/views.py
@@ -8,6 +8,10 @@ from .models import Post, Tag
 
 @login_required
 def index(request):
+    # 팔로잉 한 사람들이 쓴 post 글들의 목록 가져오기
+    post_list = Post.objects.all()\
+        .fillter(auth__in=request.user.following_set.all())     # 팔로잉 한 사람들, 이 경우 내글은 않보임..
+
     # 팔로워/팔로잉 User 목록을 조사
     # User.objects.all() 이렇게 보다는 get_user_model() 을 통해 가져오는 것이 바람직
     suggested_user_list = get_user_model().objects.all()\
@@ -15,6 +19,7 @@ def index(request):
         .exclude(pk__in=request.user.following_set.all())[0:3]   # 이미 팔로우 한 사람들은 제외
 
     return render(request, "instagram/index.html", {
+        "post_list": post_list,
         "suggested_user_list": suggested_user_list,
     })
 
