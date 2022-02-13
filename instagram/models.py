5d8125e0e6ffca3f80c0f91eeb79e3b35c16cb98
zalphee <zlaphee@gmail.com>
2022-02-13 15:28:56 +0900

    models.post 에 시간관련 필드 2개 추가 및 마이그레이 까지...

--- instagram/models.py
+++ instagram/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100, blank=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.caption
 
